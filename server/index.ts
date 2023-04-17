1b59778a7efd25ce7f05aa992d4295576c649231
Myllaume <guillaume.brioudes@myllaume.fr>
2023-04-17 22:30:12 +0200

    feat: boot and fetch libraries

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { appInformations } from '../types';
+import { AppConfig, makeConfigDir, AppConfigParams } from './appConfig.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,22 +12,40 @@ const port = 8000;
 
 app.use(express.static(path.join(__dirname, '../views/')));
 
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../views/dev.html'));
-});
+async function bootApp(): Promise<AppConfigParams> {
+  const canLoadConfig = await AppConfig.isLoadable();
+  if (canLoadConfig === false) {
+    await makeConfigDir();
+    const configToSave = new AppConfig({ libraries: [] });
+    await configToSave.save();
+  }
+  const config = await AppConfig.load();
+  return config;
+}
 
-app.get('/api', (req: Request, res: Response) => {
-  const result = {
-    app: {
-      name: 'Masteï',
-      version: '0.1.0',
-    } as appInformations,
-  };
+bootApp().then((appConfig) => {
+  app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../views/dev.html'));
+  });
 
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(result));
-});
+  app.get('/api/libraries', (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(appConfig.libraries));
+  });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  app.get('/api/appconfig', (req: Request, res: Response) => {
+    const result = {
+      app: {
+        name: 'Masteï',
+        version: '0.1.0',
+      } as appInformations,
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  });
+
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
 });
