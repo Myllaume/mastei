d50dc01c2529e51d692aa6aad5d7af5d72fb716d
Myllaume <guillaume.brioudes@myllaume.fr>
2023-04-22 13:06:52 +0200

    feat: card with date

--- views/components/datedisplay.tsx
+++ views/components/datedisplay.tsx
@@ -1,28 +1,24 @@
 interface DateDisplayProps {
   timestamp: number;
+  className?: string;
   options?: Intl.DateTimeFormatOptions;
 }
 
-// short and ling ?
-
-function DateDisplay({ timestamp, options = {} }: DateDisplayProps) {
+function DateDisplay({ timestamp, className, options = {} }: DateDisplayProps) {
   const date = new Date(timestamp);
 
+  // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#syntax
   const formatter = new Intl.DateTimeFormat('fr-FR', {
-    ...options,
-    // weekday: 'long',
     year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    // hour: 'numeric',
-    // minute: 'numeric',
-    // second: 'numeric',
-    timeZone: 'Europe/Paris', // set the timezone to French timezone
+    month: '2-digit',
+    day: '2-digit',
+    timeZone: 'Europe/Paris', // set to French timezone
+    ...options,
   });
 
   const formattedDate = formatter.format(date);
 
-  return <div>{formattedDate}</div>;
+  return <label className={className}>{formattedDate}</label>;
 }
 
 export { DateDisplay };
